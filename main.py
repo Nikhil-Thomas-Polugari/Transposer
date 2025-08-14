07bac8df0db373823a8c28b84496994a7b263554
Nikhil-Thomas-Polugari <67023514+Nikhil-Thomas-Polugari@users.noreply.github.com>
2025-08-14 20:34:52 +0000

    Removed unneeded comments

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ import os
 
 def main():
   UI.main_menu()
-  # functions.split_and_move_files("Music_File.txt", "Music_Notes")
-  # dest_folder = os.path.join(os.getcwd(), "Music_Notes")
-  # dest_file = os.path.join(dest_folder, "Saagilapadi mrokkada.txt")
-  # print(functions.detect_key_signature(dest_file))
 
 if __name__ == '__main__':
   main()
